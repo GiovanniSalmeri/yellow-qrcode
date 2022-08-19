337bd2c373e109b51927eba8f2b0b944856c4ebd
Giovanni <g.salmeri@mondodomani.org>
2022-08-19 17:51:07 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# yellow-qrcode
-QR codes
+Qrcode 0.8.20
+=============
+QR codes.
+
+<p align="center"><img src="qrcode-screenshot.png?raw=true" width="795" height="836" alt="Screenshot"></p>
+
+## How to add a QR code
+
+Create a `[qrcode]` shortcut, alone in a paragraph.
+
+The following arguments are available, all but the first argument are optional:
+ 
+`Content` = content of the QR code, words into quotes if there are spaces  
+`Label` = label of the QR code, wrap multiple words into quotes  
+`Style` = style, e.g. `left`, `center`, `right`  
+`Size` = QR code width and height in pixel  
+
+## Examples
+
+Adding a QR code, with standard or custom label:
+
+    [qrcode https://datenstrom.se/yellow/]
+    [qrcode https://datenstrom.se/yellow/ "Go to the [Yellow website]!"]
+
+Adding a QR code, different styles:
+
+    [qrcode https://datenstrom.se/yellow/ - left]
+    [qrcode https://datenstrom.se/yellow/ - center]
+    [qrcode https://datenstrom.se/yellow/ - right]
+
+Adding a QR code, different sizes:
+
+    [qrcode https://datenstrom.se/yellow/ - right 100]
+    [qrcode https://datenstrom.se/yellow/ - right 200]
+
+QR codes can also be used for geographic locations, phone calls, email messages, SMS messages, vCards, iCal events, WiFi identifiers and passwords:
+
+    [qrcode geo:41.85181,12.62127]
+    [qrcode tel:+39-06-12345678]
+    [qrcode "mailto:smith@example.com?subject=Request of help"]
+    [qrcode "SMSTO:+39-06-12345678:Hello there!"]
+    [qrcode "BEGIN:VCARD\nFN:John Smith\nEMAIL:smith@example.com\nEND:VCARD"]
+    [qrcode "BEGIN:VEVENT\nSUMMARY:Yellow Fest\nDTSTART:20220818T220000Z\nDTEND:20220819T220000Z\nEND:VEVENT"]
+    [qrcode WIFI:T:WPA;S:MyWifi;P:w65s9s67kshqw;;]
+
+However, not all QR scanners will properly interpret all these types.
+
+## Settings
+
+The following settings can be configured in file `system/extensions/yellow-system.ini`:
+
+`QrcodeSize` (default: `150`) = default width and height in pixels  
+`QrcodeColor` (default: `000000`) = RGB color  
+`QrcodeBackground` (default: `FFFFFF`) = RGB background color  
+`QrcodeShortLinkLength` (default: `30`) = maximum number of characters of @shortlink in labels  
+`QrcodeCache` (default: `qrcodes/`) = directory where codes are stored  
+
+## Installation
+
+[Download extension](https://github.com/GiovanniSalmeri/yellow-qrcode/archive/master.zip) and copy zip file into your `system/extensions` folder. Right click if you use Safari.
+
+This extension uses [QR Code Generator](https://goqr.me/api/). The service provider [does not store or log QR code contents](https://goqr.me/privacy-safety-security/). "QR Code" is a trademark of Denso Wave Incorporated.
+
+## Developer
+
+Giovanni Salmeri. [Get help](https://github.com/GiovanniSalmeri/yellow-qrcode/issues).
---
+++ qrcode.php
@@ -0,0 +1,101 @@
+<?php
+// Qrcode extension, https://github.com/GiovanniSalmeri/yellow-qrcode
+
+class YellowQrcode {
+    const VERSION = "0.8.20";
+    public $yellow;         // access to API
+
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+        $this->yellow->system->setDefault("qrcodeSize", "150");
+        $this->yellow->system->setDefault("qrcodeColor", "000000");
+        $this->yellow->system->setDefault("qrcodeBackground", "FFFFFF");
+        $this->yellow->system->setDefault("qrcodeShortLinkLength", "30");
+        $this->yellow->system->setDefault("qrcodeCache", "qrcodes/");
+    }
+
+    // Handle page content of shortcut
+    public function onParseContentShortcut($page, $name, $text, $type) {
+        $output = null;
+        if ($name=="qrcode" && ($type=="block")) {
+            list($content, $label, $style, $size) = $this->yellow->toolbox->getTextArguments($text);
+            $content = stripcslashes($content);
+            $content = preg_replace('/\R/', "\n", $content);
+            if (empty($label)) {
+                $qrTypes = [
+                    "https://"=>"Url",
+                    "http://"=>"Url",
+                    "mailto:"=>"Email",
+                    "begin:vcard\n"=>"Card",
+                    "begin:vevent\n"=>"Event",
+                    "geo:"=>"Geo",
+                    "tel:"=>"Call",
+                    "smsto:"=>"Sms",
+                    "wifi:"=>"Wifi",
+                ];
+                foreach ($qrTypes as $qrType=>$value) {
+                    if (preg_match('@^'.$qrType.'@i', $content)) {
+                        $label = $this->yellow->language->getTextHtml("qrcodeLabel".$value);
+                        break;
+                    }
+                }
+            }
+            $link = $shortLink = null;
+            $color = $this->yellow->system->get("qrcodeColor");
+            $background = $this->yellow->system->get("qrcodeBackground");
+            $fileName = $this->yellow->system->get("qrcodeCache").md5(rawurlencode($content)."@".$color."@".$background);
+            $location = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreMediaLocation").$fileName;
+            $path = $this->yellow->lookup->findMediaDirectory("coreMediaLocation").$fileName;
+            if (preg_match('@^(http://|https://|mailto:|geo:|tel:)(.+)@i', $content, $matches)) {
+                $link = $content;
+                $maxLength = $this->yellow->system->get("qrcodeShortLinkLength");
+                $shortLink = mb_strlen($matches[2])<=$maxLength ? $matches[2] : mb_substr($matches[2], 0, $maxLength-1)."…";
+            } elseif (preg_match('@^begin:(vcard|vevent)$@mi', $content, $matches)) {
+                $contentExtension = strtolower($matches[1])=="vcard" ? "vcf" : "ical";
+                $content = preg_replace('/\R/', "\r\n", $content)."\r\n";
+                if (!file_exists($path.".".$contentExtension)) {
+                    $this->yellow->toolbox->createFile($path.".".$contentExtension, $content, true);
+                }
+                $link = $location.".".$contentExtension;
+                if (preg_match('@^(fn|summary):(.+)@mi', $content, $matches)) {
+                    $shortLink = $matches[2];
+                }
+            } elseif (preg_match('@^SMSTO:(.+?):(.+)$@i', $content, $matches)) {
+                $shortLink = $matches[1];
+                $link = $matches[2];
+            } elseif (preg_match('@^WIFI:T:.+?;S:(.+?);P:(.+?);@i', $content, $matches)) {
+                // TODO: colon and semicolon can be escaped
+                $shortLink = $matches[1];
+                $link = $matches[2];
+            }
+            $formattedLabel = $label;
+            if (preg_match('@^(.*)\[(.+)\](.*)$@', $formattedLabel, $matches)) {
+                $formattedLabel = $matches[1]."<a href=\"".htmlspecialchars($link)."\">".$matches[2]."</a>".$matches[3];
+            }
+            $formattedLabel = str_replace([ "@shortlink", "@link" ], [ htmlspecialchars($shortLink), htmlspecialchars($link) ], $formattedLabel);
+            if (empty($size)) $size = $this->yellow->system->get("qrcodeSize");
+            if (!file_exists($path.".png")) {
+                $qrcodeImage = file_get_contents("http://api.qrserver.com/v1/create-qr-code/?color=".rawurlencode($color)."&bgcolor=".rawurlencode($background)."&data=".rawurlencode($content)."&qzone=2&margin=0&size=500x500&ecc=L&format=png");
+                if (substr($qrcodeImage, 0, 8)=="\211PNG\r\n\032\n") $this->yellow->toolbox->createFile($path.".png", $qrcodeImage, true);
+            }
+            $output .= "<figure class=\"qrcode";
+            if (!empty($style)) $output .= " ".htmlspecialchars($style);
+            $output .= "\">\n";
+            $output .= "<img alt=\"QR code\" width=\"".htmlspecialchars($size)."\" height=\"".htmlspecialchars($size)."\" aria-hidden=\"true\" src=\"".$location.".png"."\" />\n";
+            $output .= "<figcaption>".$formattedLabel."</figcaption>\n";
+            $output .= "</figure >\n";
+        }
+        return $output;
+    }
+
+    // Handle page extra data
+    public function onParsePageExtra($page, $name) {
+        $output = null;
+        if ($name=="header") {
+            $extensionLocation = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreExtensionLocation");
+            $output = "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}qrcode.css\" />\n";
+        }
+        return $output;
+    }
+}
